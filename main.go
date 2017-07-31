5e86f40ccaa7fd1edd4313bcbaa06cd5afb58796
gefracto <evgeniy.kostrika@gmail.com>
2017-07-31 03:09:19 +0300

    struct to gob-binary

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
 import (
+	"encoding/gob"
+	"fmt"
+	"os"
+)
+
+// Fields must be exported
+type T struct {
+	Data  string
+	Data2 int
+	Addr  *T
+}
+
+func makedb(name string) {
+
+	file, _ := os.Create(name)
+	defer file.Close()
+
+	t := T{
+		Data:  "hello",
+		Data2: 17,
+		Addr:  nil,
+	}
+	t2 := T{
+		Data:  "world",
+		Data2: 42,
+		Addr:  &t,
+	}
+	enc := gob.NewEncoder(file)
+	enc.Encode(&t2)
+}
+
+func readdb(name string) {
+	file, _ := os.Open(name)
+	defer file.Close()
+	tt := T{}
+
+	dec := gob.NewDecoder(file)
+	file.Seek(0, 0)
+
+	dec.Decode(&tt)
+
+	tt2 := tt.Addr
+	fmt.Println(tt.Data, tt.Data2)
+	fmt.Println(tt2.Data, tt2.Data2)
+}
+
+func main() {
+	makedb("mydatabase")
+	readdb("mydatabase")
+}
+
+/*
+import (
 	"archive/zip"
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -10,6 +65,11 @@ import (
 	"strconv"
 	"time"
 )
+
+type Str struct {
+	Name string
+	File []byte
+}
 
 func makeALotOfFiles(source []byte, count int) (names []string) {
 	fmt.Println("\n### Generating files ###\n")
@@ -27,7 +87,7 @@ func makeALotOfFiles(source []byte, count int) (names []string) {
 }
 
 func makeZip(name string, files []string) {
-	/*
+
 		buf := new(bytes.Buffer)
 		w := zip.NewWriter(buf)
 		defer w.Close()
@@ -38,7 +98,7 @@ func makeZip(name string, files []string) {
 			f.Write([]byte(body))
 			fmt.Println("Adding file # " + ind + " to archive")
 		}
-	*/
+
 	fmt.Println("\n### Adding files to ZIP ###\n")
 	start := time.Now()
 	newfile, _ := os.Create(name)
@@ -62,7 +122,17 @@ func makeZip(name string, files []string) {
 
 func main() {
 	tolstoy, _ := ioutil.ReadFile("warandpeace")
-	files := makeALotOfFiles(tolstoy, 20)
-	makeZip("files/myfiles.zip", files)
+	buf := new(bytes.Buffer)
+	var STR Str
+	STR.Name = "tolstoy"
+	STR.File = tolstoy
+
+	binary.Write(buf, binary.LittleEndian, STR)
+
+	ioutil.WriteFile("tolst", buf.Bytes(), 0777)
+	var q []byte
+	binary.Read(buf, binary.LittleEndian, q)
+	fmt.Print(q)
 
 }
+*/
